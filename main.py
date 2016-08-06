7af4319ed3f2df1a01824319785c10e11e6e48a1
PJB3005 <pieterjan.briers@gmail.com>
2016-08-06 19:16:58 +0200

    List servers & Logging.
    Turns out print() doesn't work when used in git bash???

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import aioprocessing.connection
 import asyncio
 import discord
+import logging
 
+logging.basicConfig(filename="example.log", level=logging.DEBUG)
 client = discord.Client()
 
 @client.async_event
 def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+    logging.info("Logged in as")
+    logging.info(client.user.name)
+    logging.info(client.user.id)
+    logging.info("Connected servers:")
+    for server in client.servers:
+        logging.info(server.name)
 
-client.run('nope')
+    logging.info("------")
+
+logging.debug("Starting client.")
+client.run("test")
