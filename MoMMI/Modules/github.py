285c57e4eda7ee7e54b214aca74ce3fa4e77d649
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2017-09-01 23:40:39 +0200

    Commit linking for github module.

--- MoMMI/Modules/github.py
+++ MoMMI/Modules/github.py
@@ -1,20 +1,14 @@
-
-
-import aiohttp
-import asyncio
 import json
 import logging
 import re
-import os
-import subprocess
-from typing import re as typing_re
+from typing import re as typing_re, Dict, Any, Tuple, List
+from urllib.parse import quote
+import aiohttp
 from colorhash import ColorHash
 from discord import Colour, Embed, Message, User
-from urllib.parse import quote
 from MoMMI.commloop import comm_event
-from typing import Dict, Any
 from MoMMI.commands import always_command
-from MoMMI.server import MServer, MChannel
+from MoMMI.server import MChannel
 from MoMMI.master import master
 from MoMMI.Modules.irc import irc_transform
 
@@ -22,7 +16,7 @@ logger = logging.getLogger(__name__)
 
 REG_PATH = re.compile(r"\[(.+?)(#L\d+(?:-L\d+)?)?\]", re.I)
 REG_ISSUE = re.compile(r"\[#?([0-9]+)\]")
-REG_BRACKETS = re.compile(r"\[(.+?)\]")
+REG_COMMIT = re.compile(r"\[([0-9a-f]{40})\]", re.I)
 
 COLOR_GITHUB_RED = Colour(0xFF4444)
 COLOR_GITHUB_GREEN = Colour(0x6CC644)
@@ -31,7 +25,7 @@ MAX_BODY_LENGTH = 200
 MD_COMMENT_RE = re.compile(r"<!--.*-->", flags=re.DOTALL)
 DISCORD_CODEBLOCK_RE = re.compile(r"```(?:([^\n]*)\n)?(.*)```", flags=re.DOTALL)
 
-REQUEST_HEADERS = {"Authorization": f"token {master.config.modules['github']['token']}"}
+REQUEST_HEADERS = {"Authorization": f"token {master.config.get_module('github', 'token')}"}
 
 
 def github_url(sub: str) -> str:
@@ -91,39 +85,35 @@ async def issue(channel: MChannel, match: typing_re.Match, message: Message):
 
     async with aiohttp.ClientSession(headers=REQUEST_HEADERS) as session:
         for match in REG_ISSUE.finditer(message.content):
-            id = int(match.group(1))
-            if id < 10:
+            issueid = int(match.group(1))
+            if issueid < 10:
                 continue
 
-            url = github_url(f"/repos/{repo}/issues/{id}")
+            url = github_url(f"/repos/{repo}/issues/{issueid}")
             async with session.get(url) as resp:
-                content = json.loads(await resp.text())
+                content = await resp.json()
 
             # God forgive me.
             embed = Embed()
             emoji = ""
-            if content.get("pull_request") is not None:
-                if content["state"] == "open":
-                    emoji = "<:PRopened:245910125041287168>"
-                    embed.colour = COLOR_GITHUB_GREEN
-                else:
-                    url = github_url(f"/repos/{repo}/pulls/{id}")
-                    async with session.get(url) as resp:
-                        prcontent = json.loads(await resp.text())
-                        if prcontent["merged"]:
-                            emoji = "<:PRmerged:245910124781240321>"
-                            embed.colour = COLOR_GITHUB_PURPLE
-                        else:
-                            emoji = "<:PRclosed:246037149839917056>"
-                            embed.colour = COLOR_GITHUB_RED
+            if content["state"] == "open":
+                emoji = "<:PRopened:245910125041287168>"
+                embed.colour = COLOR_GITHUB_GREEN
+
+            elif content.get("pull_request") is not None:
+                url = github_url(f"/repos/{repo}/pulls/{issueid}")
+                async with session.get(url) as resp:
+                    prcontent = await resp.json()
+                    if prcontent["merged"]:
+                        emoji = "<:PRmerged:245910124781240321>"
+                        embed.colour = COLOR_GITHUB_PURPLE
+                    else:
+                        emoji = "<:PRclosed:246037149839917056>"
+                        embed.colour = COLOR_GITHUB_RED
 
             else:
-                if content["state"] == "open":
-                    emoji = "<:ISSopened:246037149873340416>"
-                    embed.colour = COLOR_GITHUB_GREEN
-                else:
-                    emoji = "<:ISSclosed:246037286322569216>"
-                    embed.colour = COLOR_GITHUB_RED
+                emoji = "<:ISSclosed:246037286322569216>"
+                embed.colour = COLOR_GITHUB_RED
 
             embed.title = emoji + content["title"]
             embed.url = content["html_url"]
@@ -145,18 +135,18 @@ async def issue(channel: MChannel, match: typing_re.Match, message: Message):
             async with session.get(url, params={"recursive": 1}) as resp:
                 tree = json.loads(await resp.text())
 
-            paths = []  # type: List[str]
+            paths = []  # type: List[Tuple[str, Optional[int], Optional[int]]]
             for match in REG_PATH.finditer(message.content):
                 path = match.group(1).lower()
                 # logger.info(path)
                 paths.append(path)
 
-            for hash in tree["tree"]:
+            for filehash in tree["tree"]:
                 # logger.debug(hash["path"])
 
                 for path in paths:
-                    if hash["path"].lower().endswith(path):
-                        thepath = hash["path"]  # type: str
+                    if filehash["path"].lower().endswith(path):
+                        thepath = filehash["path"]  # type: str
                         html_url = f"https://github.com/{repo}"
                         # logger.info(html_url)
                         # logger.info(branchname)
@@ -174,6 +164,29 @@ async def issue(channel: MChannel, match: typing_re.Match, message: Message):
 
                         await channel.send(embed=embed)
                         paths.remove(path)
+
+        for match in REG_COMMIT.finditer(message.content):
+            sha = match.group(1)
+            url = github_url(f"/repos/{repo}/git/commits/{sha}")
+            async with session.get(url) as resp:
+                if resp.status != 200:
+                    continue
+
+                commit = await resp.json()
+
+            split = commit["message"].split("\n")
+            title = split[0]
+            desc = "\n".join(split[1:])
+            if len(desc) > MAX_BODY_LENGTH:
+                desc = desc[:MAX_BODY_LENGTH] + "..."
+
+            embed = Embed()
+            embed.set_footer(text=f"{repo} {sha} by {commit['author']['name']}")
+            embed.url = commit["html_url"]
+            embed.title = title
+            embed.description = desc
+
+            await channel.send(embed=embed)
 
 
 @irc_transform("convert_code_blocks")
