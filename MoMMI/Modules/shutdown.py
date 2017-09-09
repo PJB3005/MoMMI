86863f063b9d9e970fc3ba1a4494deb603a323aa
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2017-09-10 01:41:55 +0200

    Fix shutdown command.

--- MoMMI/Modules/shutdown.py
+++ MoMMI/Modules/shutdown.py
@@ -1,10 +1,10 @@
+from typing import Match
+from discord import Message
 from MoMMI.commands import command
+from MoMMI.channel import MChannel
+from MoMMI.role import MRoleType
 
-@command("shutdown", "shutdown", roles=["owner"])
-async def shutdown_command(channel, match, message):
+@command("shutdown", "shutdown", roles=[MRoleType.OWNER])
+async def shutdown_command(channel: MChannel, match: Match, message: Message):
     await channel.send("Shutting down!")
     await channel.server.master.shutdown()
-
-@command("test", "shit")
-async def plane(channel, match, message):
-    await channel.send(random.choice(channel.module_config(__name__, "responses")))
