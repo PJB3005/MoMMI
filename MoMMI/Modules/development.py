b624552af638652147ca8b5e49ca109a4723dca1
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2017-09-10 01:14:28 +0200

    Fix dev commands using string roles.

--- MoMMI/Modules/development.py
+++ MoMMI/Modules/development.py
@@ -3,12 +3,13 @@ from typing import re as typing_re
 from MoMMI.commands import command
 from MoMMI.master import master
 from MoMMI.server import MChannel
+from MoMMI.role import MRoleType
 
-@command("reload", "reload", roles=["owner"])
+@command("reload", "reload", roles=[MRoleType.OWNER])
 async def reload(channel: MChannel, match: typing_re.Match, message: Message):
     await master.reload_modules()
 
-@command("modules", "modules", roles=["owner"])
+@command("modules", "modules", roles=[MRoleType.OWNER])
 async def modules(channel: MChannel, match: typing_re.Match, message: Message):
     msg = "```"
     for module in channel.server.master.modules.values():
