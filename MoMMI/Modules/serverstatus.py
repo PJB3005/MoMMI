49380553da976d1835c9d56f474cb5d4f662bc01
Pieter-Jan briers <pieterjan.briers+git@gmail.com>
2017-09-15 22:39:14 +0200

    Adds server status module.

---
+++ MoMMI/Modules/serverstatus.py
@@ -0,0 +1,83 @@
+import asyncio
+import logging
+import struct
+from typing import Match, Union, Any
+from urllib.parse import parse_qs
+from discord import Message
+from MoMMI.commands import command
+from MoMMI.channel import MChannel
+
+logger = logging.getLogger(__name__)
+
+@command("serverstatus", r"stat(?:us|su)\s*(\S*)")
+async def serverstatus_command(channel: MChannel, match: Match, message: Message):
+    try:
+        config = channel.server_config("modules.serverstatus")
+    except:
+        # No config for this server probably.
+        await channel.send("No status configuration for this Discord server!")
+        return
+
+    servername = match.group(1) or config.get("default")
+
+    if not servername:
+        await channel.send("No target server provided.")
+        return
+
+    server_config = config[servername]
+    address = server_config["address"]
+    port = server_config["port"]
+
+    await channel.server.master.client.add_reaction(message, "⌛")
+
+    try:
+        response = await asyncio.wait_for(server_topic(address, port, b"?status"), timeout=5)
+
+    except asyncio.TimeoutError:
+        await channel.send("Server timed out.")
+        return
+
+    except:
+        await channel.send("Unknown error occured.")
+        return
+
+    mapname = response["map_name"][0]
+    players = response["players"][0]
+
+    await channel.send(f"{players} players online, map is {mapname}.")
+
+async def server_topic(address: str, port: int, message: bytes) -> Any:
+    if message[0] != 63:
+        message = b"?" + message
+
+    # Send a packet to trick BYOND into doing a world.Topic() call.
+    # https://github.com/N3X15/ss13-watchdog/blob/master/Watchdog.py#L582
+    packet = b"\x00\x83"
+    packet += struct.pack(">H", len(message) + 6)
+    packet += b"\x00" * 5
+    packet += message
+    packet += b"\x00"
+
+    reader, writer = await asyncio.open_connection(address, port)
+    writer.write(packet)
+
+    await writer.drain()
+
+    if await reader.read(2) != b"\x00\x83":
+        raise IOError("BYOND server returned data invalid.")
+
+    # Read response
+    size = struct.unpack(">H", await reader.read(2))[0]
+    response = await reader.read(size)
+    logger.info(response)
+    writer.close()
+
+    return parse_qs(decode_packet(response))
+
+# Turns the BYOND packet into either a string or a float.
+def decode_packet(packet: bytes) -> Union[float, str]:
+    if packet[0] == 0x2a:
+        return struct.unpack(">f", packet[1:5])[0]
+
+    elif packet[0] == 0x06:
+        return packet[1:-1].decode("ascii")
