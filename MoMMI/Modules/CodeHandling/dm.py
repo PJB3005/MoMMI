42fed5e84b8480b414c5ce606628124d78956b51
Pieter-Jan briers <pieterjan.briers+git@gmail.com>
2018-06-19 21:18:57 +0200

    Improve speed of DM code exec by doing reactions async.

--- MoMMI/Modules/CodeHandling/dm.py
+++ MoMMI/Modules/CodeHandling/dm.py
@@ -100,7 +100,7 @@ class DMCodeHandler(MCodeHandler):
 
         me = channel.server.get_server().me
 
-        await channel.server.master.client.add_reaction(message, "⌛")
+        asyncio.ensure_future(channel.server.master.client.add_reaction(message, "⌛"))
 
         try:
             firejail: List[str] = []
@@ -146,8 +146,8 @@ class DMCodeHandler(MCodeHandler):
                 await channel.send(embed=embed)
                 return
 
-            await channel.server.master.client.remove_reaction(message, "⌛", me)
-            await channel.server.master.client.add_reaction(message, "🔨")
+            asyncio.ensure_future(channel.server.master.client.remove_reaction(message, "⌛", me))
+            asyncio.ensure_future(channel.server.master.client.add_reaction(message, "🔨"))
 
             proc = await asyncio.create_subprocess_exec(*firejail, self.dd_executable_path(channel), dmepath + "b", "-invisible", "-ultrasafe", stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
             try:
@@ -172,16 +172,16 @@ class DMCodeHandler(MCodeHandler):
             embed.add_field(name="Execution Output",
                             value=f"```{log}```", inline=False)
 
-            await channel.server.master.client.remove_reaction(message, "🔨", channel.server.get_server().me)
+            asyncio.ensure_future(channel.server.master.client.remove_reaction(message, "🔨", channel.server.get_server().me))
 
             if fail_reason:
                 embed.color = COLOR_RUN_FAIL
                 embed.description = fail_reason
-                await channel.server.master.client.add_reaction(message, "❌")
+                asyncio.ensure_future(channel.server.master.client.add_reaction(message, "❌"))
 
             else:
                 embed.color = COLOR_RUN_SUCCESS
-                await channel.server.master.client.add_reaction(message, "✅")
+                asyncio.ensure_future(channel.server.master.client.add_reaction(message, "✅"))
 
             await channel.send(embed=embed)
 
