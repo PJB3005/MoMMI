0d6d1e23a46f611494aa181d562f9af0031b386a
PJBot <pieterjan.briers+bot@gmail.com>
2020-06-15 00:29:26 +0200

    Improve based

--- MoMMI/Modules/based.py
+++ MoMMI/Modules/based.py
@@ -9,11 +9,22 @@ async def based(channel: MChannel, _match: Match, message: Message) -> None:
     if not channel.server_config("based.enabled", True):
         return
 
-    match = re.search(r"\S\s+(based)[\s*?.!)]*$", message.content, re.IGNORECASE)
+    match = re.search(r"^\s*(based|gebaseerd|basé)[\s*?.!)]*$", message.content, re.IGNORECASE)
     if match is None:
         return
+    
+    based = "Based on what?"
+    unbased = "Not Based."
 
+    if match.group(1).lower() == "gebaseerd":
+        based = "Gebaseerd op wat?"
+        unbased = "Niet Gebaseerd."
+        
+    elif match.group(1).lower() == "basé":
+        based = "Sur quoi?"
+        unbased = "Pas basé."
+        
     if random.random() > 0.005:
-        await channel.send("Based on what?")
+        await channel.send(based)
     else:
-        await channel.send("Not Based.")
+        await channel.send(unbased)
