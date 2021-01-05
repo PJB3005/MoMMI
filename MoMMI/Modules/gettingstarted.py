76d292e3a60406921a8b794919021e8868ddd731
SweptWasTaken <sweptwastaken@protonmail.com>
2021-01-05 00:50:12 -0800

    Deleted old commands

--- MoMMI/Modules/gettingstarted.py
+++
@@ -1,7 +0,0 @@
-from typing import Match
-from discord import Message
-from MoMMI import MChannel, master, command
-
-@command("gettingstarted", "gettingstarted")
-async def dance(channel: MChannel, match: Match, message: Message) -> None:
-    await channel.send("https://hackmd.io/@ss14/getting-set-up")
--- MoMMI/Modules/howdoicode.py
+++
@@ -1,7 +0,0 @@
-from typing import Match
-from discord import Message
-from MoMMI import MChannel, master, command
-
-@command("howdoicode", "howdoicode")
-async def dance(channel: MChannel, match: Match, message: Message) -> None:
-    await channel.send("https://hackmd.io/@ss14/howdoicode")
